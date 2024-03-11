4e3b3070feb9960997afeeae7194e193af2d4ed4
jdrli <123104479+jdrli@users.noreply.github.com>
2024-03-10 23:35:00 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -9,50 +9,38 @@ namespace Assn2
             //Test constructor
             Rope rope1 = new Rope("This_is_a_rope._This_is_a_test_for_this_rope.");
 
-            //Test Insert
+            //Test Insert (also tests Split, Concatenate, Rebalance)
             //rope1.Insert("Hi", 0);
             //rope1.PrintRope();
             //Console.WriteLine(rope1.ToString());
 
-            //Test Delete
+            //Test Delete (also tests Split, Concatenate, Rebalance)
 
 
-            //Test SubString
+            //Test SubString (also tests Split, Concatenate, Rebalance)
 
 
-            //Test Find
-            Console.WriteLine("'hello!' appears at index " + rope1.Find("hello!"));
+            ////Test Find
+            //Console.WriteLine("'hello!' appears at index " + rope1.Find("hello!"));
 
-            //Test CharAt
-            Console.WriteLine(rope1.CharAt(50));
+            ////Test CharAt
+            //Console.WriteLine(rope1.CharAt(50));
 
-            //Test IndexOf
-            Console.WriteLine("'z' appears at an index of: " + rope1.IndexOf('z'));
+            ////Test IndexOf
+            //Console.WriteLine("'z' appears at an index of: " + rope1.IndexOf('z'));
 
-            //Test Reverse
-            rope1.Reverse();
-            Console.WriteLine("After reversal: \n" + rope1.ToString());
+            ////Test Reverse
+            //rope1.Reverse();
+            //Console.WriteLine("After reversal: \n" + rope1.ToString());
 
-            //Test Length
-            Console.WriteLine("This rope is " + rope1.Length() + " characters long.");
+            ////Test Length
+            //Console.WriteLine("This rope is " + rope1.Length() + " characters long.");
 
-            //Test ToString
-            Console.WriteLine(rope1.ToString());
+            ////Test ToString
+            //Console.WriteLine(rope1.ToString());
 
-            //Test PrintRope
-            rope1.PrintRope();
-
-            //Test Build
-
-
-            //Test Concatenate
-
-
-            //Test Split
-
-
-            //Test Rebalance
-
+            ////Test PrintRope
+            //rope1.PrintRope();
 
             Console.ReadLine();
         }
